f9df9b983ca06d0d9064920afd70f0fb9c776673
yu-lin23 <50695343+yu-lin23@users.noreply.github.com>
2019-05-20 15:23:13 +0200

    Updated

--- libft.h
+++ libft.h
@@ -9,5 +9,12 @@
 
 int		ft_putchar(char c);
 void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+char	*ft_strcpy(char *source, char *destination);
+char	*ft_strncpy(char *dest, char *src, size_t n);
+char	*ft_strdup(char *src);
+char	*ft_strcat(char *s1, const char*s2);
+char	*ft_strncat(char *dest, char *src, int n);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
