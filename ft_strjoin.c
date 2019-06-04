181dff6a09a999d3f86e32daf55aa8f62a958b44
yu-lin23 <50695343+yu-lin23@users.noreply.github.com>
2019-06-04 16:50:00 +0200

    Created on 04/06/19

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,35 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 14:59:35 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/05/31 15:19:09 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/06/04 16:45:34 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*	ft_strjoin(char const *s1, char onst *s2)
+char	*	ft_strjoin(char const *s1, char const *s2)
 {
+	char *new;
+	int len1;
+	int len2;
+	int i;
+	int j;
 
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	new = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	i = 0
+	while (i <= len1)
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j <= len2)
+	{
+		new[len1 + j] = s2[j];
+		j++;
+	}
+	new[len1 + len2 + 1] = '\0';
+	return (new);
 }
