f408efb0d33f48bd932e5cfb089b638bbb3b3516
yu-lin23 <50695343+yu-lin23@users.noreply.github.com>
2019-06-13 11:00:26 +0200

    Updated on 13/06/2019

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,22 +6,40 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 10:50:56 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/05/28 12:44:28 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/06/11 17:19:38 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int ft_length(long n)
+{
+	size_t i;
+	
+	i = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		i++;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
 char *ft_itoa(int n)
 {
 	char	*str;
 	int		i;
-	int		len;
-	int		nb;
+	unsigned long	len;
 
-	if ((str = (char*)malloc(sizeof(char) * len)) == NULL)
+	i = 0;
+	len = ft_length(n);
+	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-
 	if (n == 0)
 	{
 		str[0] = '0';
@@ -30,14 +48,15 @@ char *ft_itoa(int n)
 	if (n < 0)
 	{
 		str[0] = '-';
-		nb *= -1;
+		n *= -1;
 		len++;
 	}
 	while (n > 0)
 	{
 		str[i] = 48 + (n % 10);
-		nb /= 10;
+		n /= 10;
 		i--;
 	}
+	*str = '\0';
 	return (str);
 }
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,22 +6,89 @@
 /*   By: yu-lin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 14:26:05 by yu-lin            #+#    #+#             */
-/*   Updated: 2019/06/10 15:55:14 by yu-lin           ###   ########.fr       */
+/*   Updated: 2019/06/13 10:13:59 by yu-lin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+/*char	**ft_strsplit(char const *s, char c)
+{
+	char **multipleWords;
+	int i;
+	int nb;
+	char *newstr;
+
+	newstr = writewords(s, c);
+	i = 0;
+
+	while (s[i] && j < words)
+	{
+
+	}
+}*/
+
 int countwords(char const *str, char c)
 {
-	unsigned char s;
+	int i;
+	int j;
+	int numberOfWords;
 
-	s = ft_strtrim(str);
-	printf("%s", (unsigned char *)s);
-	//printf("number of words is %d", numberOfWords);
+	i = 0;
+	j = 0;
+	numberOfWords = 0;
+	while (str[i] != '\0')
+	{
+		if (str[0] == c)
+		{
+			i++;
+			continue;
+		}
+		j++;
+		if (str[j] == '\0')
+		{
+			numberOfWords++;
+			break;
+		}
+		else if (str[i] == c && str[j] != c )
+			numberOfWords++;
+		i++;
+	}
+	printf("Number of words is %d", numberOfWords);
 	return (0);
 }
+
+/*char *writewords(char *str, char c)
+{
+	char *newWord;
+	int i;
+	int j;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
+			newWord[j] = str[i];
+			i++;
+			j++;
+			while (str[i] == c)
+				i++;
+		}
+		else
+		{
+			newWord[j] = string[i];
+			i++;
+			j++;
+		}
+	}
+	if (str[i - 1] != c)
+	{
+		newWord[j] = c;
+		j++;
+	}
+	newWord[j] = '\0';
+	return (newWord)
+}*/
 
 int main()
 {
